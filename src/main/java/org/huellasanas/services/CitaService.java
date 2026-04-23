2768f4896b32b47071c0728249edca1a81bbe779
sixorca00-collab <sixorca00@gmail.com>
2026-04-23 17:35:12 -0500

    feat(CitaService): add dates for customer

--- src/main/java/org/huellasanas/services/CitaService.java
+++ src/main/java/org/huellasanas/services/CitaService.java
@@ -2,13 +2,16 @@ package org.huellasanas.services;
 
 import org.huellasanas.DAO.Impl.CitasDAO;
 import org.huellasanas.models.Cita;
+import org.huellasanas.models.Mascota;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CitaService {
     private final CitasDAO citDAO = new CitasDAO();
+    private final MascotaService mascotaService = new MascotaService();
 
     public boolean agendarCita(Cita cita) {
         //No permitir citas para el pasado
@@ -32,14 +35,13 @@ public class CitaService {
     }
     //Update
     public boolean actualizarCita(Cita cita){
-        if (cita.getId() < 0 ){
-            System.out.println("Ingrese numero valido");
+        if (cita.getId() <= 0 ){
         }
         return false;
     }
     //Delete
     public boolean eliminarCita(int id){
-        if (id < 0 ){
+        if (id <= 0 ){
             System.out.println("Ingrese numero valido");
             return false;
         }
@@ -54,4 +56,13 @@ public class CitaService {
             c.getVeterinarioId().equals(newCita.getVeterinarioId()) && c.getFechaHora().equals(newCita.getFechaHora())
         );
     }
+
+    //Cita por cliente
+   public List<Cita> citasPorCliente(String clienteId){
+        List<Mascota> mascotas = mascotaService.listarMascotasPorCliente(clienteId);
+        List<Integer> idsMascotas = mascotas.stream().map(Mascota::getId).toList();
+
+        return citDAO.findAll().stream().filter(c -> idsMascotas.contains(c.getMascotaId())).toList();
+
+   }
 }
