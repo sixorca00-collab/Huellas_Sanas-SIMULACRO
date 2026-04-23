67d386e81fe95c8c727d80e65541bb7bd02e0d03
sixorca00-collab <sixorca00@gmail.com>
2026-04-23 17:13:31 -0500

    feat(MascotaService): add logic pets

---
+++ src/main/java/org/huellasanas/services/MascotaService.java
@@ -0,0 +1,37 @@
+package org.huellasanas.services;
+
+import org.huellasanas.DAO.Impl.MascotaDAO;
+import org.huellasanas.models.Mascota;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MascotaService {
+    private final MascotaDAO mDao= new MascotaDAO();
+    //Logica crear mascota
+    public boolean crearMascota(Mascota m){
+        if (m == null) return false;
+        return mDao.save(m);
+    }
+    //Listar mascota
+    public List<Mascota> listarMascotas(){
+        return mDao.findAll();
+    }
+
+    //Buscar mascota por id
+    public Optional<Mascota> buscarMascotaPorId(int id){
+        return mDao.findById(id);
+    }
+
+    //Actualizar mascota
+    public boolean actualizarMascota(Mascota m){
+        if (m == null) return false;
+        return mDao.update(m);
+    }
+
+    //Eliminar mascota
+    public boolean eliminarMascota(int id){
+        return mDao.delete(id);
+    }
+    }
+
