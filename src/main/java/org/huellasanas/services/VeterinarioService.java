b9ed943a48f618c9589f14c90b2c6bdde215a938
sixorca00-collab <sixorca00@gmail.com>
2026-04-23 17:46:26 -0500

    feat(CitaService): add dates for vet and separation by hour

--- src/main/java/org/huellasanas/services/VeterinarioService.java
+++ src/main/java/org/huellasanas/services/VeterinarioService.java
@@ -1,6 +1,8 @@
 package org.huellasanas.services;
 
+import org.huellasanas.DAO.Impl.CitasDAO;
 import org.huellasanas.DAO.Impl.VeterinarioDAO;
+import org.huellasanas.models.Cita;
 import org.huellasanas.models.Veterinario;
 
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.Optional;
 
 public class VeterinarioService {
     private final VeterinarioDAO vDAO = new VeterinarioDAO();
+    private final CitasDAO cDAO = new CitasDAO();
 
     public boolean crearVeterinario(Veterinario v){
         if (v == null)return false;
@@ -29,5 +32,25 @@ public class VeterinarioService {
     //Eliminar
     public boolean eliminarVeterinario(String id){
         return vDAO.delete(id);
+    }
+
+    // Choque de citas
+    public boolean choqueCitas(Cita nueva){
+        //Validamos datos
+        if (nueva == null)return false;
+
+        List<Cita> citas = cDAO.findAll();
+        return citas.stream().anyMatch(c -> {
+            //Mismo veterinario
+            if (!c.getVeterinarioId().equals(nueva.getVeterinarioId())){return false;}
+            //Mismo dia
+            if (!c.getFechaHora().toLocalDate().equals(nueva.getFechaHora().toLocalDate())){return false;}
+
+            //Choque de tiempo 1Hora margen
+            long minutos = Math.abs(
+                    java.time.Duration.between(c.getFechaHora(), nueva.getFechaHora()).toMinutes()
+            );
+            return minutos < 60;
+        } );
     }
 }
